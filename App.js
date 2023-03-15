97631354149010b25ad177ad2e21e6cfefbd1343
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-15 00:04:37 -0300

    Conexão com o firebase, lendo dados e .env

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import firebase from "./src/firebaseConnection";
+
+console.disableYellowBox = true;
 
 export default function App() {
+  const [nome, setNome] = useState("Carregando...");
+  const [idade, setIdade] = useState("");
+
+  useEffect(() => {
+    async function dados() {
+      //Olheiro da nossa database
+      await firebase
+        .database()
+        .ref("usuarios/2")
+        .on("value", (snapshot) => {
+          setNome(snapshot.val().nome);
+          setIdade(snapshot.val().idade);
+        });
+
+      // await firebase.database().ref('nome').once('value', (snapshot)=> {
+      //   setNome(snapshot.val());
+      // });
+    }
+    dados();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 25 }}>
+      <Text style={{ fontSize: 25 }}>Olá {nome}</Text>
+      <Text style={{ fontSize: 25 }}>Idade {idade}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/firebaseConnection.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import {
+  APIKEY,
+  AUTHDOMAIN,
+  DATABASEURL,
+  PROJECTID,
+  STORAGEBUCKET,
+  MESSAGINGSENDERID,
+  APPID,
+} from "@env";
+
+let firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  databaseURL: DATABASEURL,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID,
+};
+
+if (!firebase.apps.length) {
+  //Abrir minha conexao
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
