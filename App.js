d7886565645f0b071ed6e1a64afb22c8a43a4bac
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-15 13:19:12 -0300

    Criando sistema de cadastro de usuário com email e senha

--- App.js
+++ App.js
@@ -1,121 +1,137 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Button,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import firebase from "./src/firebaseConnection";
 
-import Listagem from "./src/Listagem";
+// import Listagem from "./src/Listagem";
 
 console.disableYellowBox = true;
 
 export default function App() {
-  const [nome, setNome] = useState();
-  const [idade, setIdade] = useState();
-  const [cargo, setCargo] = useState();
-  const [usuarios, setUsuarios] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [nome, setNome] = useState();
+  // const [idade, setIdade] = useState();
+  // const [cargo, setCargo] = useState();
+  // const [usuarios, setUsuarios] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-  useEffect(() => {
-    async function dados() {
-      //Olheiro da nossa database - atualiza imediatamente
-      // await firebase
-      //   .database()
-      //   .ref("usuarios/2")
-      //   .on("value", (snapshot) => {
-      //     setNome(snapshot.val().nome);
-      //     setIdade(snapshot.val().idade);
-      //   });
-      // Atualiza quando o app é renderizado
-      // await firebase.database().ref('nome').once('value', (snapshot)=> {
-      //   setNome(snapshot.val());
-      // });
-      // Criando um nó
-      //await firebase.database().ref('tipo').set('clientes');
-      //Removendo um nó
-      //await firebase.database().ref("usuario").remove();
-      //Criando um child (registro do nó)
-      // await firebase.database().ref('usuarios').child(3).set({
-      //   nome: 'Leticia',
-      //   idade: 17,
-      //   cargo: 'Estudante do ensino médio'
-      // })
-      //Atualiza somente um campo do registro
-      // await firebase
-      //   .database()
-      //   .ref("usuarios")
-      //   .child(3)
-      //   .update({ cargo: "Universitária" });
-      //Buscando todos os registros
-      await firebase
-        .database()
-        .ref("usuarios")
-        .on("value", (snapshot) => {
-          setUsuarios([]);
-          snapshot.forEach((childItem) => {
-            let data = {
-              key: childItem.key,
-              nome: childItem.val().nome,
-              idade: childItem.val().idade,
-              cargo: childItem.val().cargo,
-            };
-            // forma de atualizar o array com a inversão da ordem da lista (reverse())
-            setLoading(false);
-            setUsuarios((oldArray) => [...oldArray, data].reverse());
-          });
-        });
-    }
-    dados();
-  }, []);
+  // useEffect(() => {
+  //   async function dados() {
+  //Olheiro da nossa database - atualiza imediatamente
+  // await firebase
+  //   .database()
+  //   .ref("usuarios/2")
+  //   .on("value", (snapshot) => {
+  //     setNome(snapshot.val().nome);
+  //     setIdade(snapshot.val().idade);
+  //   });
+  // Atualiza quando o app é renderizado
+  // await firebase.database().ref('nome').once('value', (snapshot)=> {
+  //   setNome(snapshot.val());
+  // });
+  // Criando um nó
+  //await firebase.database().ref('tipo').set('clientes');
+  //Removendo um nó
+  //await firebase.database().ref("usuario").remove();
+  //Criando um child (registro do nó)
+  // await firebase.database().ref('usuarios').child(3).set({
+  //   nome: 'Leticia',
+  //   idade: 17,
+  //   cargo: 'Estudante do ensino médio'
+  // })
+  //Atualiza somente um campo do registro
+  // await firebase
+  //   .database()
+  //   .ref("usuarios")
+  //   .child(3)
+  //   .update({ cargo: "Universitária" });
+  //Buscando todos os registros
+  //   await firebase
+  //     .database()
+  //     .ref("usuarios")
+  //     .on("value", (snapshot) => {
+  //       setUsuarios([]);
+  //       snapshot.forEach((childItem) => {
+  //         let data = {
+  //           key: childItem.key,
+  //           nome: childItem.val().nome,
+  //           idade: childItem.val().idade,
+  //           cargo: childItem.val().cargo,
+  //         };
+  //         // forma de atualizar o array com a inversão da ordem da lista (reverse())
+  //         setLoading(false);
+  //         setUsuarios((oldArray) => [...oldArray, data].reverse());
+  //       });
+  //     });
+  // }
+  // dados();
+  // }, []);
 
   async function cadastrar() {
-    if (nome !== "" && idade !== "" && cargo !== "") {
-      let usuarios = await firebase.database().ref("usuarios");
-      let chave = usuarios.push().key;
+    // if (nome !== "" && idade !== "" && cargo !== "") {
+    //   let usuarios = await firebase.database().ref("usuarios");
+    //   let chave = usuarios.push().key;
+    //   usuarios.child(chave).set({
+    //     nome: nome,
+    //     idade: idade,
+    //     cargo: cargo,
+    //   });
+    //   alert("Cadastro realizado com sucesso");
+    //   setNome("");
+    //   setIdade("");
+    //   setCargo("");
+    // }
 
-      usuarios.child(chave).set({
-        nome: nome,
-        idade: idade,
-        cargo: cargo,
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((value) => {
+        alert("Usuário cadastrado: " + value.user.email);
+      })
+      .catch((error) => {
+        if (error.code === "auth/weak-password") {
+          alert("Sua senha deve ter pelo menos 6 caracteres");
+          return;
+        }
+        if (error.code === "auth/invalid-email") {
+          alert("Email inválido");
+          return;
+        } else {
+          alert("Algo deu errado...");
+          return;
+        }
       });
-
-      alert("Cadastro realizado com sucesso");
-      setNome("");
-      setIdade("");
-      setCargo("");
-    }
+    setEmail("");
+    setPassword("");
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Nome</Text>
+      <Text style={styles.text}>Email</Text>
       <TextInput
         style={styles.input}
         underlineColorAndroid="transparent"
-        onChangeText={(texto) => setNome(texto)}
-        value={nome}
+        onChangeText={(texto) => setEmail(texto)}
+        value={email}
+        textContentType='emailAddress'
       />
-      <Text style={styles.text}>Idade</Text>
+      <Text style={styles.text}>Senha</Text>
       <TextInput
         style={styles.input}
         underlineColorAndroid="transparent"
-        onChangeText={(texto) => setIdade(texto)}
-        value={idade}
+        onChangeText={(texto) => setPassword(texto)}
+        textContentType='newPassword'
+        value={password}
       />
-      <Text style={styles.text}>Cargo</Text>
+      {/*<Text style={styles.text}>Cargo</Text>
       <TextInput
         style={styles.input}
         underlineColorAndroid="transparent"
         onChangeText={(texto) => setCargo(texto)}
         value={cargo}
-      />
-      <Button title="Novo cadastro" onPress={cadastrar} />
-      {loading ? (
+      /> */}
+      <Button title="Cadastrar" onPress={cadastrar} />
+      {/* {loading ? (
         <ActivityIndicator style={styles.indicator} color="#121212" size={35} />
       ) : (
         <FlatList
@@ -123,7 +139,7 @@ export default function App() {
           data={usuarios}
           renderItem={({ item }) => <Listagem data={item} />}
         />
-      )}
+      )} */}
     </View>
   );
 }
@@ -146,6 +162,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   indicator: {
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
